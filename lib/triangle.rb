88501ebab7ac55bfdfc73b7d2db70ea013c66ff9
Bobby Neal <rsneal01@gmail.com>
2020-06-29 17:34:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,10 +14,7 @@ class Triangle
   
   def kind
    if length1 == 0 && length2 == 0 && length3 == 0
-      begin
         raise TriangleError
-        rescue TriangleError
-      end
     elsif length1 == length2 && length2 == length3
       :equilateral
     elsif length2 == length3 || length1 == length3 || length1 == length2
