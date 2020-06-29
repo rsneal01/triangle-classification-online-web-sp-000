90bae1ae082bd3b1a4d1333c2e7f51ad518bfd44
Bobby Neal <rsneal01@gmail.com>
2020-06-29 17:04:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,7 +21,8 @@ class Triangle
       :isosceles
     elsif length1 != length2 || length2 != length3 || length1 != length3
       :scalene
-    elsif 
+    elsif length1 == 0 || length2 == 0 || length3 == 0
+      
     end
   end
   
